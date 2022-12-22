532fe1ad8360edc56b67a922ead3a630b5a4e80f
Karan Singh <112791089+KaranSingh2666@users.noreply.github.com>
2022-12-22 18:13:51 +0530

    Update list.js

--- server/models/list.js
+++ server/models/list.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose'); // Erase if already required
-// Declare the Schema of the Mongo model
-var listSchema = new mongoose.Schema({
+const mongoose = require('mongoose'); 
+
+const listSchema = new mongoose.Schema({
     title:{
         type:String,
         required:true,
